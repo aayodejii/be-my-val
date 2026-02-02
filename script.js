a0a7cce521f4e74328d3aa10ff700c47024aeda3
Ayodeji Akenroye <e.manuel_07@yahoo.com>
2026-02-02 10:14:27 +0100

    initial project setup with basic structure

---
+++ script.js
@@ -0,0 +1,16 @@
+// Valentine's Day Interactive Page
+// Main elements
+const yesBtn = document.getElementById('yesBtn');
+const noBtn = document.getElementById('noBtn');
+
+// Yes button click handler
+yesBtn.addEventListener('click', () => {
+    console.log('Yes clicked!');
+    // TODO: Show celebration
+});
+
+// No button click handler
+noBtn.addEventListener('click', () => {
+    console.log('No clicked!');
+    // TODO: Mobile behavior
+});
